653b078c3347264cbb2250cf1fb164457b904bfa
priyanshu <priyanshu.sharma_cs.ccv21@gla.ac.in>
2025-01-28 13:53:32 +0530

    removes duplicates from an array code completed

---
+++ RemoveDuplicates.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class RemoveDuplicates {
+    static void Main() {
+        int[] arr={1,2,1,3,2,3,1};
+        int n=arr.Length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    arr[j]=-1;
+                }
+            }
+        }
+        Console.WriteLine("Array after removing duplicates: ");
+        for(int i=0;i<n;i++){
+            if(arr[i]!=-1){
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
